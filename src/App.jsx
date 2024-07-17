762267f921134a52c916860a00df038ba79dfbf6
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-07-17 22:49:44 +0530

    bug-fixes

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,12 @@ import './App.css'
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 import { Outlet } from 'react-router-dom'
-import Button from './Components/Button';
 
 function App() {
   return (
     <>
    <Navbar />
     <Outlet />
-    <Button disabled />
     </>
     
   )
