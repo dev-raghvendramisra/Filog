ab407499e9e1c616f6cb0bad0ef21ae1bc83cd88
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-08-11 23:34:30 +0530

    redeploying cloud func

--- src/Cloud Functions/conf/conf.js
+++ src/Cloud Functions/conf/conf.js
@@ -5,7 +5,7 @@ const conf = {
     blogCollectionID: String(process.env.APPWRITE_BLOG_COLLECTION_ID),
     userProfilesCollectionID: String(process.env.APPWRITE_USERPROFILE_COLLECTION_ID),
     bucketId: String(process.env.APPWRITE_BUCKET_ID),
-    apiKey:String(process.env.APPWRITE_CLOUD_FUNCTION_API_KEY),
+    apiKey:String(process.env.APPWRITE_API_KEY),
 }
 
 export default conf;
--- src/Cloud Functions/index.js
+++ src/Cloud Functions/index.js
@@ -6,3 +6,4 @@ export default async function handler({req,res}){
 
 }
 
+
