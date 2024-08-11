569b821ece45fd4ffae84b15c8813d925af816ff
Raghvendra Misra <itsraghav12@gmail.com>
2024-08-12 03:23:50 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ import DatabaseService from "./Services/dbService.js";
 export default async function handler({req,res,log}){
   const dbService = new DatabaseService()
   log(req)
-  return res.send(<div>hello</div>) 
+  return res.send("<div>hello</div>") 
 } 
 
 
