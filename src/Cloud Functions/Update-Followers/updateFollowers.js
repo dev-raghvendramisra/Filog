bcb41ed0b9b7c32bbfd713ebac5dbf0de4212807
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-08-12 00:00:04 +0530

    redeploying cloud func

--- src/Cloud Functions/Update-Followers/updateFollowers.js
+++
@@ -1,4 +0,0 @@
-import conf from "../conf/conf";
-export default async function updateFollowers({targetUserId, userId, followingArray, targetUserProfile, currentUserProfile}){
-   
-}
--- src/Cloud Functions/index.js
+++
@@ -1,9 +0,0 @@
-import { dbServices } from "./Services/dbService";
-
-export default async function handler({req,res}){
-
-  console.log(req);
-
-}
-
-
