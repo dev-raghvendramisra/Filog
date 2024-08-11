c042c3a1ea2cb02e213256e9dce075eda0b09ec5
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-08-12 01:51:19 +0530

    playing with cloud functions

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,4 @@
-import DatabaseService from "./Services/dbService";
+import DatabaseService from "./Services/dbService.js";
 export default async function handler({req,res}){
   const dbService = new DatabaseService()
   return res.send(req)
