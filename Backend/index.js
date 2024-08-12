02b99fd9d02ae7f1a5f07c33b4e06fa09a4e3eeb
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-08-12 08:19:30 +0530

    bug-fixes

--- Backend/index.js
+++ Backend/index.js
@@ -13,25 +13,25 @@ export default async function handler({ req, res, log }) {
         // Check if the event type includes user profile updates
         if (evtType.includes(conf.userProfilesCollectionID) && evtType.includes("update")) {
             // Parse the updated attribute from the request body
-            const stageActionJson = req.body.stageAction ? req.body.stageAction : null;
-            if (!stageActionJson) {
+            const stagedActionJson = req.body.stagedAction ? req.body.stagedAction : null;
+            if (!stagedActionJson) {
                 log("This is not the event to perform operations");
                 return res.empty();
             }
 
             // Convert updated attribute JSON to an object
-            const stageAction = JSON.parse(stageActionJson);
-            log("Updated Attribute:", stageAction);
+            const stagedAction = JSON.parse(stagedActionJson);
+            log("Updated Attribute:", stagedAction);
 
             // Process the update if it's a follow or unfollow event
-            if (stageAction.type === "following" || stageAction.type === "unfollowing") {
-                log("Processing update for:", stageAction);
+            if (stagedAction.type === "following" || stagedAction.type === "unfollowing") {
+                log("Processing update for:", stagedAction);
 
                 // Call the function to update followers
                 const updationRes = await updateFollowers({
-                    targetUserId: stageAction.value,
+                    targetUserId: stagedAction.value,
                     userId: req.body.userId,
-                    type: stageAction.type,
+                    type: stagedAction.type,
                     log: log
                 });
 
@@ -39,12 +39,12 @@ export default async function handler({ req, res, log }) {
 
                 // Log the result of the update operation
                 if (updationRes.ok) {
-                    log(`${req.body.userName} (${req.body.userId}) ${stageAction.type} ${stageAction.value}`);
+                    log(`${req.body.userName} (${req.body.userId}) ${stagedAction.type} ${stagedAction.value}`);
                 } else {
                     log("Failed to update followers");
                 }
             } else {
-                log("Invalid update type:", stageAction.type);
+                log("Invalid update type:", stagedAction.type);
             }
         } else {
             log("Event not related to user profile updates");
