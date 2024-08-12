45e17a2d95b0a7c4bef272eba6d185e3bf4df0b5
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-08-12 06:30:08 +0530

    playing with cloud functions

--- Backend/index.js
+++ Backend/index.js
@@ -23,7 +23,7 @@ export default async function handler({req,res,log}){
           })
           log(updationRes)
           if(updationRes.ok){
-            log(req.body.userName,"-",req.body.userId,"started following",updatedAttribute.value)
+            log(req.body.userName,"-"+req.body.userId+"started following"+updatedAttribute.value)
             return res.empty()
           }
           else{
