d1eb5a037aa776fdef84b3466a83d11b8be49b2e
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-09-01 01:19:58 +0530

    some bugs fid

--- Frontend/src/pages/Email-Verification/EmailVerification.jsx
+++ Frontend/src/pages/Email-Verification/EmailVerification.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import { authErrHandler, getNewVerificationEmail, startAuthentication } from '../../utils';
 import { authServices } from '../../services';
@@ -7,71 +7,93 @@ import { Button, FeedbackMessage, GenToast } from '../../components';
 import toast from 'react-hot-toast';
 import { ID } from 'appwrite';
 import { useDispatch, useSelector } from 'react-redux';
-import { login,logout } from '../../store/authSlice';
+import { login, logout } from '../../store/authSlice';
 
 function EmailVerification() {
+  // State hooks for handling error, success message, and various other states
   const [searchParams] = useSearchParams();
   const [err, setErr] = React.useState(null);
   const [successMsg, setSuccessMsg] = React.useState(null);
   const [disabled, setDisabled] = React.useState(true);
   const [resCode, setResCode] = React.useState(null);
   const [timer, setTimer] = React.useState(0);
+  
+  // Redux and other hooks
   const { isUserLoggedIn, userData, fetching } = useSelector((state) => state.auth);
   const navigate = useNavigate();
   const { isDark } = useTheme();
   const dispatch = useDispatch();
-  const id = ID.unique()
- 
-
+  const id = ID.unique();
+  
+  // Error messages
   const errMsg = ["Invalid verification link", "Email already verified"];
   
+  // Function to check if the verification link has expired
   const isVerificationExpired = React.useCallback((expire) => {
     const crrDate = new Date();
     const expireDate = new Date(expire.replace(" ", "T") + "Z");
     return crrDate >= expireDate;
-  },[]);
-
-  const handleClick = React.useCallback(async()=>{
+  }, []);
+  
+  // Function to handle the "Get new" button click
+  const handleClick = React.useCallback(async () => {
     setDisabled(true);
     setErr(null);
-    if(resCode!==401 && !isUserLoggedIn){
-      const timer = setTimeout(()=>navigate("/login"),7000)
-      setTimer(timer)
+    
+    if (resCode !== 401 && !isUserLoggedIn) {
+      const timer = setTimeout(() => navigate("/login"), 7000);
+      setTimer(timer);
       return setErr("You need to login first");
     }
-    const res = await getNewVerificationEmail({isUserLoggedIn, userData,setErr,errMsg, navigate, timer, setTimer});
-    res 
-    ? (()=>{
+    
+    const res = await getNewVerificationEmail({
+      isUserLoggedIn,
+      userData,
+      setErr,
+      errMsg,
+      navigate,
+      timer,
+      setTimer
+    });
+    
+    if (res) {
       toast.custom(<GenToast type="successMsg">Verification email sent successfully</GenToast>);
-      setSuccessMsg("Verification email sent , click on the link to verify");
+      setSuccessMsg("Verification email sent, click on the link to verify");
       setErr(null);
-      })()
-    : null
-  },[resCode,isUserLoggedIn,userData])
-
-  const verification = React.useCallback(async(userId, secret) => {
-      
+    }
+  }, [resCode, isUserLoggedIn, userData]);
+  
+  // Function to verify the email using userId and secret
+  const verification = React.useCallback(async (userId, secret) => {
     const res = await authServices.verifyEmail(userId, secret);
-    const didErrOccured = authErrHandler({ res, setErr, navigate, errMsg, setResCode, verification:true,setTimer });
+    const didErrOccured = authErrHandler({
+      res,
+      setErr,
+      navigate,
+      errMsg,
+      setResCode,
+      verification: true,
+      setTimer
+    });
+    
     if (!didErrOccured) {
       toast.custom(<GenToast type="success">Email verified successfully</GenToast>);
       setSuccessMsg("Email verified successfully");
       setErr(null);
-      clearTimeout(timer)
-      const newTimer = setTimeout(()=>navigate("/"),7000)
-      startAuthentication({ dispatch, login,logout, navigate });
-      setTimer(newTimer)
+      clearTimeout(timer);
+      const newTimer = setTimeout(() => navigate("/"), 7000);
+      startAuthentication({ dispatch, login, logout, navigate });
+      setTimer(newTimer);
       setDisabled(true);
     }
-  },[])
-
-
-  useEffect(() => {
+  }, []);
+  
+  // Effect to handle the email verification process on component mount
+  React.useEffect(() => {
     const userId = searchParams.get('userId');
     const secret = searchParams.get('secret');
     const expire = searchParams.get('expire');
-  
-
+    
     if (userId && secret && !isVerificationExpired(expire)) {
       verification(userId, secret);
     } else if (userId && secret) {
@@ -81,7 +103,7 @@ function EmailVerification() {
     }
   }, [searchParams]);
   
-
+  // Effect to handle error messages and cleanup timer
   React.useEffect(() => {
     if (err) {
       setSuccessMsg(null);
@@ -89,34 +111,27 @@ function EmailVerification() {
     }
     return () => clearTimeout(timer);
   }, [err]);
-
-
-  React.useEffect(()=>{
-      setDisabled(fetching);
-  },[fetching])
-
-
   
-
-
-
-
-
+  // Effect to handle disabling of button based on fetching state
+  React.useEffect(() => {
+    setDisabled(fetching);
+  }, [fetching]);
+  
   return (
     <div className='h-100vh w-full flex items-center justify-center' id={id + "email-verification-wrapper"}>
-      <div className='w-fit h-fit flex items-center justify-start p-4vw flex-col  border-2  rounded-3xl border-gray-200 dark:border-footer_text_light' id={id + "email-verification-cont"}>
+      <div className='w-fit h-fit flex items-center justify-start p-4vw flex-col border-2 rounded-3xl border-gray-200 dark:border-footer_text_light' id={id + "email-verification-cont"}>
         <div className='flex items-center flex-col' id={id + "email-verification-header"}>
           <img className='w-8vw' src={isDark ? "/icons/filogXgmail-dark.webp" : "/icons/filogXgmail-light.webp"} />
-          <p className='text-1.7vw mt-0.5vw' id={id+"email-verification-heading"}>Email verification</p>
+          <p className='text-1.7vw mt-0.5vw' id={id + "email-verification-heading"}>Email verification</p>
           <p className='text-0.5vw text-footer_text'>Verifying Your Email, You can request new link if it fails</p>
           {err && <FeedbackMessage err={err} className='mt-1vw'>{err}</FeedbackMessage>} 
-          {successMsg && <FeedbackMessage err={false} className='mt-1vw'>{successMsg}</FeedbackMessage> }
+          {successMsg && <FeedbackMessage err={false} className='mt-1vw'>{successMsg}</FeedbackMessage>}
         </div>
         <div className='flex gap-4 mt-2vw' id={id + "email-verification-buttons"}>
-          <Button outline className='text-1.2vw' onClick={
-            () => {
-             clearTimeout(timer)
-             navigate("/")}}>
+          <Button outline className='text-1.2vw' onClick={() => {
+            clearTimeout(timer);
+            navigate("/");
+          }}>
             Do it later
           </Button>
           <Button primary={!disabled} disabled={disabled} onClick={handleClick} className='text-1.2vw'>
@@ -129,5 +144,3 @@ function EmailVerification() {
 }
 
 export default EmailVerification;
-
-
