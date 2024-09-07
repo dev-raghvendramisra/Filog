35fa2a740464d2b45f3370d2d7f792ac97cac324
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-09-07 22:14:55 +0530

    chore: Add console log for debugging purposes

--- Frontend/api/metatags.js
+++ Frontend/api/metatags.js
@@ -3,6 +3,8 @@ import path from 'path';
 
 export default async function handler(req, res) {
    const {id} = req.query;
+   console.log(id);
+   
    res.setHeader('Content-Type', 'application/json');
    res.send(id)
 }
