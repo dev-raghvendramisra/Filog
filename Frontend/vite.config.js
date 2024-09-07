544cca16b20e3168380425b898c258606f37a3a6
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-09-07 08:14:39 +0530

    refactor: Update build configuration for lazy loading and asset file naming

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    manifest: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]', // For CSS and other assets
+        entryFileNames: 'assets/[name].[hash].js',        // For JS files
+      }
+    }
   },
   server: {
     historyApiFallback: true
