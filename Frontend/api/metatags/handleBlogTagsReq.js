01d815a595b2feee946963ee66f2e820bcd2331c
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-09-08 06:25:10 +0530

    bug-fixes

--- Frontend/api/metatags/handleBlogTagsReq.js
+++ Frontend/api/metatags/handleBlogTagsReq.js
@@ -1,4 +1,4 @@
-import {Client, Databases} from 'appwrite'
+import {Client, Databases} from 'node-appwrite'
 import conf from '../conf/conf.js';
 import {getBlogMetatags} from '../utils/index.js';
 
