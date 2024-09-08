efff6c8344602f8f790e639eaaf1e350363b40f4
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-09-08 06:50:28 +0530

    bug-fixes

--- Frontend/api/utils/getViteAssets.js
+++ Frontend/api/utils/getViteAssets.js
@@ -1,8 +1,13 @@
 import {promises as fs} from 'fs';
-import path from 'path';
+import { fileURLToPath } from 'url';
+import {dirname, join} from 'path';
 
 export default async function getViteAssets() {
-    const manifestPath = path.join(__dirname, '../../.vite/manifest.json');
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename)
+
+    const manifestPath = join(__dirname, '../../.vite/manifest.json');
    try {
 
     const data = await fs.readFile(manifestPath, 'utf-8');
