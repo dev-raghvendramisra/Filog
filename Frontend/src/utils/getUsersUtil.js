4e86bd65bdea76f355e7b1f78e96f31673cfd231
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-09-22 04:18:27 +0530

    refactor: Sort users by version and priority in getUsersUtil

--- Frontend/src/utils/getUsersUtil.js
+++ Frontend/src/utils/getUsersUtil.js
@@ -4,7 +4,8 @@ import { dbServices } from "../services";
 
 export default async function getUsersUtil({userId="#",offset=0,limit=10,dispatch,clearUsers,setUsers,query=[]}){
     let queries=[
-        Query.orderDesc("priority")
+        Query.orderDesc("version"), //we use version as the first parameter to sort the users because this will list most active users first because version gets incremented upon interactions and updates
+        Query.orderDesc("priority")//we use this parameter to sort the users because this will list the users with the highest priority first by default like the founder account or the admin account
     ]
     if(query.length==0){
         queries.push(Query.notEqual("userId",[userId]))
