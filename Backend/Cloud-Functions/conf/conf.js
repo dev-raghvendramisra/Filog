573452114ccbf2056f67b24696cf910649d168b4
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-09-23 22:22:28 +0530

    bug-fixes

--- Backend/Cloud-Functions/conf/conf.js
+++ Backend/Cloud-Functions/conf/conf.js
@@ -6,9 +6,6 @@ const conf = {
     userProfilesCollectionID: String(process.env.APPWRITE_USERPROFILE_COLLECTION_ID),
     bucketId: String(process.env.APPWRITE_BUCKET_ID),
     apiKey:String(process.env.APPWRITE_API_KEY),
-    oAuthClientId: String(process.env.APPWRITE_OAUTH_CLIENT_ID),
-    oAuthClientSecret: String(process.env.APPWRITE_OAUTH_CLIENT_SECRET),
-    oAuthRefreshToken: String(process.env.APPWRITE_OAUTH_REFRESH_TOKEN),
 }
 
 export default conf;
