9299e83828c5e22cc390756990b8d1f78993071d
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-10-26 21:27:31 +0530

    minors

--- Backend/Cloud-Functions/conf/conf.js
+++ Backend/Cloud-Functions/conf/conf.js
@@ -6,6 +6,7 @@ const conf = {
     userProfilesCollectionID: String(process.env.APPWRITE_USERPROFILE_COLLECTION_ID),
     bucketId: String(process.env.APPWRITE_BUCKET_ID),
     apiKey:String(process.env.APPWRITE_API_KEY),
+    notificationCollectionID: String(process.env.APPWRITE_NOTIFICATION_COLLECTION_ID),
 }
 
 export default conf;
