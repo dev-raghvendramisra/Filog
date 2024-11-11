3f40dc448d1a11e5911de058843f80b2101ad1b3
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-11-11 22:04:08 +0530

    minors

--- Backend/Vercel-Functions/CDN/conf/conf.js
+++ Backend/Vercel-Functions/CDN/conf/conf.js
@@ -1,5 +1,7 @@
-export default conf={
+const conf={
     bucketId : String(process.env.APPWRITE_BUCKET_ID),
     appwriteUrl : String(process.env.APPWRITE_URL),
     projectId  :String(process.env.APPWRITE_PROJECT_ID)
 }
+
+export default conf
