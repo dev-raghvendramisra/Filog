9c04ff763c9732b055afac5d4c8477c9fe0cfe5a
Dev.Raghvendra Misra <itsraghav12@gmail.com>
2024-12-22 02:25:39 +0530

    Add Docker run and stop scripts, update .gitignore, and clean up environment log

--- Backend/Filog-Api/services/jwtGenerationService.js
+++ Backend/Filog-Api/services/jwtGenerationService.js
@@ -4,8 +4,7 @@ const {createJwt, mailer, envLogger:logger} = require('../libs')
 module.exports.jwtGenerationService = async function(userId, recipient, emailType="VERIFICATION_EMAIL" ,expiryDate = new Date().getTime()+60*60*1000, expiry = '1h'){
     try {
         const token = createJwt({userId}, expiry) // 1 hour from now
-        const embeddedUrl  =  `${constants.EMAIL_TYPES[emailType].FRONTED_ENDPOINT}?userId=${userId}&secret=${token}&expire=${expiryDate}`
-
+        const embeddedUrl  =  `${constants.EMAIL_TYPES[emailType].FRONTEND_ENDPOINT}?userId=${userId}&secret=${token}&expire=${expiryDate}`
         const emailRes = await mailer(emailType, { recipient, embeddedUrl })
         if (emailRes.ok) {
             logger.info("Email sent successfully")
---
+++ Backend/Filog-Api/stop.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker rm filog-api-1 filog-api-2 filog-api-3 -f
+
+echo "Containers stopped with ports 8000, 8001, and 8002."
+
+
+
+
