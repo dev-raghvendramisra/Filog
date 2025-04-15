24aab047e85c76bddba9907e7c7c8a091ea8df5c
Raghvendra Misra <itsraghav12@gmail.com>
2025-04-16 02:55:02 +0530

    Update oauthClients.ts

--- Backend/Filog-Api/src/libs/oauthClients.ts
+++ Backend/Filog-Api/src/libs/oauthClients.ts
@@ -4,6 +4,6 @@ import { OAuth2Client as GoogleOAuth2Client } from "google-auth-library";
 export const googleOAuthClient = new GoogleOAuth2Client(
   conf.GOOGLE_OAUTH_CLIENT_ID,
   conf.GOOGLE_OAUTH_SECRET,
-  "http://localhost:8000/api/v1/auth/oauth/google"
+  "https://apis.filog.in/api/v1/auth/oauth/google"
 );
 
